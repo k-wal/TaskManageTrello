4e65b7222046af749a87c08744172b966610350a
kitkat77 <stellaolive157@gmail.com>
2018-04-06 19:18:44 +0530

    Adding task db in models.py

--- models.py
+++ models.py
@@ -22,13 +22,15 @@ class User(UserMixin, db.Model):
     password_hash = db.Column('password_hash',db.String(80), nullable=False)
     remember_me = db.Column('remember_me', db.Boolean, unique=False, default=False)
 
+    tasks = db.relationship(
+        'Task',
+        backref=db.backref('user-table', lazy='joined'),
+        cascade="all, delete-orphan",
+        passive_deletes=True
+    )
+
     @classmethod
     def hash_password(cls, password):
-        print("***")
-        print("***")
-        print("***")
-        print("***")
-        print("PASSWORD: %s" % password)
         return generate_password_hash(password, method='sha256')
 
     def check_password(self,password):
@@ -43,6 +45,42 @@ class User(UserMixin, db.Model):
             'registration_time' : self.registration_time,
             'password_hash' : self.password_hash,
             'remember_me' : self.remember_me
+        }
+
+    def __repr__(self):
+        return self.serialize().__repr__()
+
+class Task(db.Model):
+
+    __tablename__ = 'task-table'
+
+    id = db.Column('task_id', db.Integer, primary_key=True, autoincrement=True)
+    start_time = db.Column('start_time', db.DateTime)
+    end_time = db.Column('end_time', db.DateTime)
+    deadline = db.Column('deadline', db.DateTime, nullable=False)
+    user_id = db.Column('user_id', db.Integer, db.ForeignKey(
+        'user-table', ondelete='CASCADE'), nullable=False)
+    name = db.Column('name', db.String(100), nullable=False)
+    description = db.Column('description', db.String(200))
+    status = db.Column('status', db.Enum(
+        *['Pending', 'Ongoing', 'Completed']), nullable=False, default='Pending')
+    priority = db.Column('priority', db.Boolean, nullable=False, default=False)
+    incentive = db.Column('incentive', db.String(200))
+    consequences = db.Column('consequences', db.String(200))
+
+    def serialize(self):
+        return {
+            'task_id': self.task_id,
+            'start_time': self.start_time,
+            'end_time': self.end_time,
+            'deadline': self.deadline,
+            'user_id': self.user_id,
+            'name': self.name,
+            'description': self.description,
+            'status': self.status,
+            'priority': self.priority,
+            'incentive': self.incentive,
+            'consequences': self.consequences
         }
 
     def __repr__(self):
