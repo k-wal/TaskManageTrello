c3c73c0112c4ab5edd141718412fafd1bcb37e59
kitkat77 <stellaolive157@gmail.com>
2018-04-06 18:21:45 +0530

    Autopep8 'cause neat

--- api/user_registration.py
+++ api/user_registration.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template,url_for,request
+from flask import Blueprint, render_template, url_for, request
 from models import User
 from flask_wtf import FlaskForm
 from wtforms import StringField, DateField, PasswordField, BooleanField
@@ -8,31 +8,40 @@ from models import db, User
 
 user_blueprint = Blueprint('user_blueprint', __name__)
 
+
 class RegisterForm(FlaskForm):
-	username = StringField('Username',validators=[InputRequired(), Length(max=32)])
-	name = StringField('Name',validators=[InputRequired(), Length(max=64)])
-	dob = DateField('Date Of Birth',validators=[InputRequired()])
-	password = PasswordField('Password',validators=[InputRequired(), Length(min=8,max=80), EqualTo('confirm')])
-	confirm = PasswordField('Confirm Password',validators=[InputRequired(),Length(min=8, max=80)])
+    username = StringField('Username', validators=[
+                           InputRequired(), Length(max=32)])
+    name = StringField('Name', validators=[InputRequired(), Length(max=64)])
+    dob = DateField('Date Of Birth', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[
+                             InputRequired(), Length(min=8, max=80), EqualTo('confirm')])
+    confirm = PasswordField('Confirm Password', validators=[
+                            InputRequired(), Length(min=8, max=80)])
+
 
 class LoginForm(FlaskForm):
-	username = StringField('username',validators=[InputRequired(), Length(max=20)])
-	password = PasswordField('password',validators=[InputRequired(),Length(min=8,max=40)])
-	remember = BooleanField('Remember Me')
+    username = StringField('username', validators=[
+                           InputRequired(), Length(max=20)])
+    password = PasswordField('password', validators=[
+                             InputRequired(), Length(min=8, max=40)])
+    remember = BooleanField('Remember Me')
 
 
-@user_blueprint.route('/user/registration', methods = ['POST','GET'])
+@user_blueprint.route('/user/registration', methods=['POST', 'GET'])
 def register_user():
     form = RegisterForm(request.form)
-    if request.method=='POST' and form.validate_on_submit():
-        new_user = User(username=form.username.data, name=form.name.data, dob=form.dob.data, password_hash=set_password(form.username.password))
+    if request.method == 'POST' and form.validate_on_submit():
+        new_user = User(username=form.username.data, name=form.name.data,
+                        dob=form.dob.data, password_hash=set_password(form.username.password))
         db.session.add(new_user)
         db.session.commit
-        return render_template('/templates/register.html',form=form)
+        return render_template('/templates/register.html', form=form)
 
     return '<h1>Oops.</h1><br><h2>Some error occured, please try again later.</h2>'
 
-@user_blueprint.route('/user/login', methods = ['POST'])
+
+@user_blueprint.route('/user/login', methods=['POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -41,7 +50,7 @@ def login():
             return '<h1>Invalid username or password</h1>'
             # return redirect(url_for('login'))
 
-        login_user(user, remember = form.remember.data)
+        login_user(user, remember=form.remember.data)
         return '<h1>Login Successful</h1>'
         # return redirect(url_for('index'))
-    return render_template('login.html', form = form)
+    return render_template('login.html', form=form)
--- models.py
+++ models.py
@@ -7,26 +7,32 @@ from sqlalchemy import engine
 db = SQLAlchemy()
 
 # noinspection PyUnusedLocal
+
+
 @event.listens_for(engine.Engine, 'connect')
 def __set_sqlite_pragma(db_conn, conn_record):
     cursor = db_conn.cursor()
     cursor.execute('PRAGMA foreign_keys=ON;')
     cursor.close()
 
+
 class User(UserMixin, db.Model):
-    
-    __tablename__='user-table'
-    
+
+    __tablename__ = 'user-table'
+
     id = db.Column('user_id', db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column('username', db.String(64), unique=True, nullable=False)
+    username = db.Column('username', db.String(64),
+                         unique=True, nullable=False)
     name = db.Column('name', db.String(120), unique=False, nullable=False)
     dob = db.Column('date_of_birth', db.DateTime, nullable=False)
-    registration_time = db.Column('registration_time',db.DateTime, nullable=False)
-    password_hash = db.Column('password_hash',db.String(80), nullable=False)
-    remember_me = db.Column('remember_me', db.Boolean, unique=False, default=False)
-    tasks=db.relationship(
+    registration_time = db.Column(
+        'registration_time', db.DateTime, nullable=False)
+    password_hash = db.Column('password_hash', db.String(80), nullable=False)
+    remember_me = db.Column('remember_me', db.Boolean,
+                            unique=False, default=False)
+    tasks = db.relationship(
         'Task',
-        backref=db.backref('user-table',lazy='joined'),
+        backref=db.backref('user-table', lazy='joined'),
         cascade="all, delete-orphan",
         passive_deletes=True
     )
@@ -39,49 +45,51 @@ class User(UserMixin, db.Model):
 
     def serialize(self):
         return {
-            'user_id' : self.user_id,
-            'username' : self.username,
-            'name' : self.name,
-            'dob' : self.dob,
-            'registration_time' : self.registration_time,
-            'password_hash' : self.password_hash,
-            'remember_me' : self.remember_me
+            'user_id': self.user_id,
+            'username': self.username,
+            'name': self.name,
+            'dob': self.dob,
+            'registration_time': self.registration_time,
+            'password_hash': self.password_hash,
+            'remember_me': self.remember_me
         }
 
     def __repr__(self):
         return self.serialize().__repr__()
+
 
 class Task(db.Model):
-    
-    __tablename__='task-table'
 
-    id=db.Column('task_id',db.Integer,primary_key=True,autoincrement=True)
-    start_time=db.Column('start_time',db.DateTime,default=NULL)
-    end_time=db.Column('end_time',db.DateTime,default=NULL)
-    deadline=db.Column('deadline',db.DateTime,nullable=False)
-    user_id=db.Column('user_id',db.Integer,db.ForeignKey('user-table',ondelete='CASCADE'),nullable=False)
-    name=db.Column('name',db.String(100),nullable=False)
-    description=db.Column('description',db.String(200))
-    status=db.Column('status',db.Enum(*['Pending','Ongoing','Completed']),nullable=False,default='Pending')
-    priority=db.Column('priority',db.Boolean,nullable=False,default=False)
-    incentive=db.Column('incentive',db.String(200))
-    consequences=db.Column('consequences',db.String(200))
+    __tablename__ = 'task-table'
+
+    id = db.Column('task_id', db.Integer, primary_key=True, autoincrement=True)
+    start_time = db.Column('start_time', db.DateTime, default=NULL)
+    end_time = db.Column('end_time', db.DateTime, default=NULL)
+    deadline = db.Column('deadline', db.DateTime, nullable=False)
+    user_id = db.Column('user_id', db.Integer, db.ForeignKey(
+        'user-table', ondelete='CASCADE'), nullable=False)
+    name = db.Column('name', db.String(100), nullable=False)
+    description = db.Column('description', db.String(200))
+    status = db.Column('status', db.Enum(
+        *['Pending', 'Ongoing', 'Completed']), nullable=False, default='Pending')
+    priority = db.Column('priority', db.Boolean, nullable=False, default=False)
+    incentive = db.Column('incentive', db.String(200))
+    consequences = db.Column('consequences', db.String(200))
 
     def serialize(self):
-        return  {
-            'task_id' : self.task_id,
-            'start_time' : self.start_time,
-            'end_time' : self.end_time,
-            'deadline' : self.deadline,
-            'user_id' : self.user_id,
-            'name' : self.name,
-            'description' : self.description,
-            'status' : self.status,
-            'priority' : self.priority,
-            'incentive' : self.incentive,
-            'consequences' : self.consequences
+        return {
+            'task_id': self.task_id,
+            'start_time': self.start_time,
+            'end_time': self.end_time,
+            'deadline': self.deadline,
+            'user_id': self.user_id,
+            'name': self.name,
+            'description': self.description,
+            'status': self.status,
+            'priority': self.priority,
+            'incentive': self.incentive,
+            'consequences': self.consequences
         }
 
     def __repr__(self):
         return self.serialize().__repr__()
-
